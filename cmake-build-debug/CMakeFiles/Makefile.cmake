f68bb3e2e4341c4d3882b9bffd4a482bc5feac4d
jackchensm <1655664358@qq.com>
2019-04-16 17:13:01 +0800

    feat(struct):append struct pointer function

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/struct4.dir/DependInfo.cmake"
+  "CMakeFiles/struct5.dir/DependInfo.cmake"
   )
