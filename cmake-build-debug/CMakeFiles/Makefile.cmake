139256121f8370f6f652e2e00c9f89ca1209e715
jackchensm <1655664358@qq.com>
2019-04-17 16:58:07 +0800

    feat(bit):append bit limit example test

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/enum2.dir/DependInfo.cmake"
+  "CMakeFiles/bit1.dir/DependInfo.cmake"
   )
---
+++ demo_union2.c
@@ -0,0 +1,41 @@
+//
+// Created by Administrator on 2019/4/17.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#define TOTAL 4  //人员总数
+struct{
+    char name[20];
+    int num;
+    char sex;
+    char profession;
+    union{
+        float score;
+        char course[20];
+    } sc;
+} bodys[TOTAL];
+int main(){
+    int i;
+    //输入人员信息
+    for(i=0; i<TOTAL; i++){
+        printf("Input info: ");
+        scanf("%s %d %c %c", bodys[i].name, &(bodys[i].num), &(bodys[i].sex), &(bodys[i].profession));
+        if(bodys[i].profession == 's'){  //如果是学生
+            scanf("%f", &bodys[i].sc.score);
+        }else{  //如果是老师
+            scanf("%s", bodys[i].sc.course);
+        }
+        fflush(stdin);
+    }
+    //输出人员信息
+    printf("\nName\t\tNum\tSex\tProfession\tScore / Course\n");
+    for(i=0; i<TOTAL; i++){
+        if(bodys[i].profession == 's'){  //如果是学生
+            printf("%s\t%d\t%c\t%c\t\t%f\n", bodys[i].name, bodys[i].num, bodys[i].sex, bodys[i].profession, bodys[i].sc.score);
+        }else{  //如果是老师
+            printf("%s\t%d\t%c\t%c\t\t%s\n", bodys[i].name, bodys[i].num, bodys[i].sex, bodys[i].profession, bodys[i].sc.course);
+        }
+    }
+    return 0;
+}
