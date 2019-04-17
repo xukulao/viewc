0c0ea64c93fb8d622e89cd04ad3954eeadc6190f
jackchensm <1655664358@qq.com>
2019-04-17 19:07:31 +0800

    feat(typedef):append typedef test example

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/bit3.dir/DependInfo.cmake"
+  "CMakeFiles/typeof.dir/DependInfo.cmake"
   )
