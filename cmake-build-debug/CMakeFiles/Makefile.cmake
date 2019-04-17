d81dd6eba103293fb0ba344b5e67577151ccaead
jackchensm <1655664358@qq.com>
2019-04-17 17:46:11 +0800

    feat(bit):append bit arithmetic example

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/bit1.dir/DependInfo.cmake"
+  "CMakeFiles/bit2.dir/DependInfo.cmake"
   )
