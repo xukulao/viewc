2169845155df140313216280388193a361deb136
jackchensm <1655664358@qq.com>
2019-04-18 15:24:20 +0800

    feat(file):append fwrite and fread function test

---
+++ file6.c
@@ -0,0 +1,36 @@
+//
+// Created by Administrator on 2019/4/18.
+//
+
+#include <stdio.h>
+#define N 10
+int main()
+{
+    char *a[N],*b[N];
+    int size = sizeof(char);
+    FILE *file;
+    char *fileName = "E:/c/demo/hello.txt";
+    file = fopen(fileName,"rb+");
+    if(file == NULL){
+        printf("file not found!");
+    }
+
+    int i;
+    for(i=0;i<N;i++){
+        scanf("%s",&a[i]);
+    }
+
+    fwrite(a,size,N,file);
+
+    rewind(file);
+
+    fread(b,size,N,file);
+
+    for(i=0;i<N;i++){
+        printf("%s",b[i]);
+    };
+
+    printf("\n");
+
+    return 0;
+}
