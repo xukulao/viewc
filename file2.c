d405fd00fe8cc6434a7100ff93cf5cb7290383f2
jackchensm <1655664358@qq.com>
2019-04-18 14:19:03 +0800

    feat(file):append fgetc function test

--- file2.c
+++ file2.c
@@ -3,8 +3,36 @@
 //
 
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
-    
+    //char *fileName = "E:/c/demo/hello.txt";
+    char *fileName = "E:/c/demo/hello.php";
+    FILE *file = fopen(fileName,"rt");
+
+    if (file == NULL){
+        printf("Error:not found file!");
+    }
+
+    char ch;
+    while((ch=fgetc(file))!=EOF){
+        putchar(ch);
+    }
+
+    putchar('\n');
+
+
+
+//    fclose(file);
+//    char cmd[100] = "php ";
+//    char *content = system(strcat(cmd,fileName));
+//
+//    printf("%s\n",*content);
+
+    if(ferror(file)){
+        printf("read error!");
+    }else{
+        printf("read success!");
+    }
+    return 0;
 }
