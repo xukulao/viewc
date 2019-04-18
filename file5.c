4ad644bac652e31df0d14feb42f25f0ac6a973de
jackchensm <1655664358@qq.com>
2019-04-18 15:01:49 +0800

    feat(file):append file fputs function test

---
+++ file5.c
@@ -0,0 +1,33 @@
+//
+// Created by Administrator on 2019/4/18.
+//
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    FILE *file;
+    char *fileName = "E:/c/demo/hello.txt";
+    file = fopen(fileName,"at+");
+    if (file == NULL){
+        printf("not found file!");
+    }
+
+    printf("input your data!\n");
+    char str[102]= {0};
+    char tempStr[100];
+    gets(tempStr);
+    strcat(str,"\n");
+    strcat(str,tempStr);
+
+    printf("str=%s\n",str);
+    if(fputs(str,file)==EOF){
+        printf("write error!");
+    }else{
+        printf("write ok!");
+    }
+    printf("end");
+    fclose(file);
+    return 0;
+
+}
