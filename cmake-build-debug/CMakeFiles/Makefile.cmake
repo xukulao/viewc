e2b9dff6acbebc922ee7ca351a86197876f30f09
jackchensm <1655664358@qq.com>
2019-04-18 11:01:55 +0800

    feat(typedef):typedef example test

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/typeof.dir/DependInfo.cmake"
+  "CMakeFiles/typedef.dir/DependInfo.cmake"
   )
