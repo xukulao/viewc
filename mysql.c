b16441aa7f33eef13c65851e43312fb459fe975b
jackchensm <1655664358@qq.com>
2019-04-19 14:08:45 +0800

    feat(master):do nothing

---
+++ mysql.c
@@ -0,0 +1,11 @@
+//
+// Created by Administrator on 2019/4/18.
+//
+#include <stdio.h>
+#include "C:\wamp64\bin\mysql\mysql5.7.24\include/mysql.h"
+#pragma comment(lib,"libmysqld.lib")
+int main()
+{
+
+}
+
