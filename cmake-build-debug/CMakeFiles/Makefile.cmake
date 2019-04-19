882f9cdc77dc3a6425d8404a355201dcaa390ced
jackchensm <1655664358@qq.com>
2019-04-19 19:35:39 +0800

    feat(module):append file test

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/debug.dir/DependInfo.cmake"
+  "CMakeFiles/module.dir/DependInfo.cmake"
   )
