81db5491e52dd77cf49e6237f86baf02dea8ed91
immortalChensm <1655664358@qq.com>
2019-04-20 23:36:38 +0800

    feat(person):append crud for person project

--- person.c
+++ person.c
@@ -13,54 +13,37 @@ typedef struct person{
     char address[100];
 } PERSON;
 
-long insert(FILE *file,PERSON person,long offset);
+long insert();
 long fSize(FILE *file);
 int getMenuid();
 void removeUser();
+void updateUser();
+void listUser();
+char *fileName = "E:/c/viewc/persondata.txt";
+char *tempFileName = "E:/c/viewc/persond_temp.txt";
+int menuid;
+FILE *file;
+FILE *tmpFileHandle;
 int main()
 {
 
-    FILE *file;
-    char *fileName = "E:/c/viewc/persondata.txt";
-    file = fopen(fileName,"rb+");
-    if (file == NULL){
-        printf("error!");
-    }
-    int menuid;
-    PERSON person;
-
-    PERSON *per = &person;
-    PERSON tempPerson;
-
-    PERSON *tmp = &tempPerson;
-
-    char *tempName = NULL;
-    char *yesNo = NULL;
-    int readCount = 0;
     while (1){
         menuid = getMenuid();
-        fseek(file,0,SEEK_SET);
         switch(menuid){
-//            case 0:
-//                printf("nothing\n");
-//                break;
+            case 0:
+               exit(0);
+                break;
             case 1:
-                printf("please enter person info!\n");
-                scanf("%s %d %d %s",per->name,&per->age,&per->num,per->address);
-
-                insert(file,*per,0);
+                insert();
                 break;
             case 2:
-                //fread(&tempPerson, sizeof(struct person),1,file);
-                //printf("%s %d %d %s\n",tmp->name,tmp->age,tmp->num,tmp->address);
-                printf("Name\t\tAge\t\tNum\t\tAddress\n");
-                while((readCount=fread(tmp, sizeof(struct person),1,file))>0){
-                    printf("%s\t\t%d\t\t%d\t\t%s\n",tmp->name,tmp->age,tmp->num,tmp->address);
-                }
-                fseek(file,0,SEEK_SET);
+                listUser();
                 break;
             case 3:
-                removeUser(file);
+                removeUser();
+                break;
+            case 4:
+                updateUser();
                 break;
             default:
 
@@ -73,14 +56,37 @@ int main()
 }
 
 
+void listUser()
+{
+
+    if(file == NULL){
+        file = fopen(fileName,"rb+");
+    }
+    fseek(file,0,SEEK_SET);
+    PERSON tempPerson;
+    PERSON *tmp = &tempPerson;
+    int readCount = 0;
+    printf("+%#5s----%5s+%5s---%5s+%5s---%5s+%5s-------%5s+\n","\x2D","\x2D","\x2D","\x2D","\x2D","\x2D","\x2D","\x2D");
+    printf("+%5sName%5s+%5sAge%5s+%5sNum%5s+%5sAddress%5s+\n","\x20","\x20","\x20","\x20","\x20","\x20","\x20","\x20");
+    if (ftell(file)>0){
+        fseek(file,0,SEEK_SET);
+    }
+    while((readCount=fread(tmp, sizeof(struct person),1,file))>0){
+        printf("|***%s***|***%d***|***%d***|***%s***|\n",tmp->name,tmp->age,tmp->num,tmp->address);
+    }
+
+    printf("+-------+-------+-------+-------+\n");
+}
+
 int getMenuid()
 {
     int menuid;
-    printf("***************************************************\n");
+
     printf("[1]:add person\n");
     printf("[2]:list person\n");
     printf("[3]:remove person\n");
-    printf("***************************************************\n");
+    printf("[4]:update person\n");
+
     scanf("%d",&menuid);
     fflush(stdin);
     return menuid;
@@ -88,13 +94,28 @@ int getMenuid()
 
 
 
-long insert(FILE *file,PERSON person,long offset)
+long insert()
 {
+    PERSON person;
+
+    PERSON *per = &person;
+    PERSON tempPerson;
+
+    PERSON *tmp = &tempPerson;
+
+    char *tempName = NULL;
+
+    int readCount = 0;
+
+    file = fopen(fileName,"rb+");
+    printf("please enter person info!\n");
+    scanf("%s %d %d %s",per->name,&per->age,&per->num,per->address);
+
     long Size = fSize(file);
     fseek(file,Size,SEEK_SET);
     fwrite(&person, sizeof(struct person),1,file);
-    fseek(file,0,SEEK_SET);
 
+    menuid = 2;
 }
 
 long fSize(FILE *file)
@@ -108,49 +129,89 @@ long fSize(FILE *file)
     return n;
 }
 
-void removeUser(FILE *file)
+void removeUser()
 {
     printf("input username who u want to remove\n");
     char temp[50];
     fflush(stdin);
     scanf("%s",temp);
-
     PERSON tempPerson;
-
-    PERSON *tmp = &tempPerson;
-
     int readCount=0;
     int nBytes = 0;
     if(temp){
-
+        file = fopen(fileName,"rb+");
+        tmpFileHandle = fopen(tempFileName,"wb+");
         fseek(file,0,SEEK_SET);
-        FILE *tmpfile;
-        char tmpfileName[50] = "E:/c/viewc/persond_temp.txt";
-        tmpfile = fopen(tmpfileName,"wb+");
-        fseek(tmpfile,0,SEEK_SET);
-        while((readCount=fread(tmp, sizeof(struct person),1,file))>0){
+        fseek(tmpFileHandle,0,SEEK_SET);
+        while((readCount=fread(&tempPerson, sizeof(struct person),1,file))>0){
             nBytes+=readCount;
-            if (tmp&&strcmp(temp,tmp->name) != 0){
-                fwrite(tmp,sizeof(struct person),1,tmpfile);
+            if (strcmp(temp,tempPerson.name) != 0){
+                fwrite(&tempPerson,sizeof(struct person),1,tmpFileHandle);
             }
         }
-        fseek(tmpfile,0,SEEK_SET);
 
-        fclose(tmpfile);
+        fclose(tmpFileHandle);
 
         //读取临时文件复制到原来的文件完成删除功能
-        tmpfile = fopen(tmpfileName,"rb+");
-        file = fopen("E:/c/viewc/persondata.txt","wb+");
-
+        tmpFileHandle = fopen(tempFileName,"rb+");
+        file = fopen(fileName,"wb+");
+        fseek(tmpFileHandle,0,SEEK_SET);
         fseek(file,0,SEEK_SET);
-        PERSON *tmp = &tempPerson;
-        while((readCount=fread(tmp, sizeof(struct person),1,tmpfile))>0){
-            printf("copy %s\n",tmp->name);
-            fwrite(tmp,sizeof(struct person),1,file);
+        PERSON tmp;
+        readCount = 0;
+        while((readCount=fread(&tmp, sizeof(struct person),1,tmpFileHandle))>0){
+            fwrite(&tmp,sizeof(struct person),1,file);
         }
-        fclose(tmpfile);
-        fseek(file,0,SEEK_SET);
+
     }
 
+    menuid = 2;
 
+}
+
+void updateUser()
+{
+    char name[50];
+    printf("input username:\n");
+    scanf("%s",name);
+    int readCount;
+    tmpFileHandle = fopen(tempFileName,"wb+");
+    file = fopen(fileName,"rb+");
+    fseek(tmpFileHandle,0,SEEK_SET);
+    fseek(file,0,SEEK_SET);
+    PERSON tempPerson;
+    PERSON data;
+    int find = 0;
+    while((readCount=fread(&tempPerson, sizeof(struct person),1,file))>0){
+       if(strcmp(name,tempPerson.name)==0){
+           find = 1;
+           printf("please input user info:\n");
+           scanf("%s %d %d %s",data.name,&data.age,&data.num,data.address);
+           fwrite(&data, sizeof(struct person),1,tmpFileHandle);
+       }else{
+           fwrite(&tempPerson, sizeof(struct person),1,tmpFileHandle);
+       }
+    }
+
+    fclose(tmpFileHandle);
+    if (find == 0){
+        printf("sorry,not found this user!\n");
+
+    }else{
+        //复制文件
+
+        tmpFileHandle = fopen(tempFileName,"rb+");
+        file = fopen(fileName,"wb+");
+        fseek(file,0,SEEK_SET);
+        fseek(tmpFileHandle,0,SEEK_SET);
+        PERSON tmp;
+        while((readCount=fread(&tmp, sizeof(struct person),1,tmpFileHandle))>0){
+            printf("update:%s",tmp.name);
+            fwrite(&tmp, sizeof(struct person),1,file);
+        }
+        printf("update userinfo ok!\n");
+
+    }
+
+    menuid = 2;
 }
