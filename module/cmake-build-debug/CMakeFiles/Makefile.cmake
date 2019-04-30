499300bdbfcf6f9de5a4b606e914250770812ff2
jackchensm <1655664358@qq.com>
2019-04-30 11:08:57 +0800

    feat(file):module head file program

--- module/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ module/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/demo.dir/DependInfo.cmake"
+  "CMakeFiles/main2.dir/DependInfo.cmake"
   )
