7cc5950dd10b80ea128a3850ffa847015b0799f8
immortalChensm <1655664358@qq.com>
2019-05-02 00:07:09 +0800

    feat(pointer):pointer function test

--- module/main5.c
+++ module/main5.c
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+int *age(int * ,int *);
 int sum()
 {
     static int n=0;
@@ -23,26 +24,59 @@ int sum()
     return n;
 }
 
-int *age(int a,int b)
+int *age(int *a,int *b)
 {
-    int *c;
-    *c=a+b;
-    return c;
+   int *c;
+   int c1;
+   c = &c1;
+   *c=*a+*b;
+   return c;
 }
 int main()
 {
     int i;
-    for(i=0;i<100;i++){
-        sum();
-    }
-    printf("****************************\n");
-    printf("n=%d\n",*(int *)(0X407030));
+//    for(i=0;i<100;i++){
+//        sum();
+//    }
+//    printf("****************************\n");
+//    printf("n=%d\n",*(int *)(0X407030));
+//
+//    int (*func)() = sum;
+//    func();
+    //printf("c=%d\n",*age(1,1));
 
-    int (*func)() = sum;
-    func();
-    printf("c=%d\n",*age(1,1));
+    //int *(*ageN)(int,int) = age;
+    //x(int x,int y)
+    //printf("c1=%#X\n",*ageN(100,200));
+    //int *result = *age(10,20);
+    //printf("age=%d\n",*result);
+    //printf("age=%d\n",*age(199,2));
+    //printf("age=%d\n",*age(100,200));
+//    int *a;
+//    int *b;
+//    int a1=10;
+//    int b1=20;
+//    a=&a1;
+//    b=&b1;
+//    *b=100;
+//    //printf("age=%d",age(a,b));
+//    printf("a=%#X,a=%d,b=%#X,b=%d\n",a,*a,b,b1);
 
-    int *(*a)(int,int) = age;
-    //printf("c1=%#X\n",*a(100,200));
+    int a1=10;
+    int b1=10;
+    int *a,*b;
+    a=&a1;
+    b=&b1;
+    printf("age=%d\n",*age(a,b));
+    a1=100;
+    b1=200;
+    printf("age=%d\n",*age(a,b));
+
+    int *(*ageNum)(int *,int *) = age;
+    a1=1000;
+    b1=2000;
+    printf("ageNum=%d\n",*ageNum(a,b));
+    a1=20;b1=20;
+    printf("ageNum=%d\n",*&*ageNum(a,b));
     return 0;
 }
