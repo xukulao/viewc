de2526fb74d6813d9c34dc12063f528bbdfb328a
immortalChensm <1655664358@qq.com>
2019-05-03 15:18:41 +0800

    feat(pointer):pointe test review

--- module/main6.c
+++ module/main6.c
@@ -6,28 +6,28 @@
 #include <stdlib.h>
 int main()
 {
-    int *pa;
-    int a=10;
-    pa=&a;
-    printf("pa=%d,pa=%#X,pa=%d\n",*pa,pa, sizeof(pa));
-
-    char b='c';
-    char *pb;
-    pb=&b;
-    printf("pb=%c,pb=%#X,pb=%d\n",*pb,pb, sizeof(pb));
-    pb="chinailoveu";
-    printf("pb=%s\n",pb);
-
-    printf("***********************\n");
-    char *pc;
-    pc="hello";
-    printf("pc=%s,pc=%d\n",pc, sizeof(pc));
-    printf("hello=%s,hello=%#X,hello=%d\n","hello","hello", sizeof("hello"));
-
-
-    printf("hello=%c\n","hello"[0]);
-    printf("hello=%c\n",*"hello"+0);
-    printf("hello=%c\n",*"hello");
+//    int *pa;
+//    int a=10;
+//    pa=&a;
+//    printf("pa=%d,pa=%#X,pa=%d\n",*pa,pa, sizeof(pa));
+//
+//    char b='c';
+//    char *pb;
+//    pb=&b;
+//    printf("pb=%c,pb=%#X,pb=%d\n",*pb,pb, sizeof(pb));
+//    pb="chinailoveu";
+//    printf("pb=%s\n",pb);
+//
+//    printf("***********************\n");
+//    char *pc;
+//    pc="hello";
+//    printf("pc=%s,pc=%d\n",pc, sizeof(pc));
+//    printf("hello=%s,hello=%#X,hello=%d\n","hello","hello", sizeof("hello"));
+//
+//
+//    printf("hello=%c\n","hello"[0]);
+//    printf("hello=%c\n",*"hello"+0);
+//    printf("hello=%c\n",*"hello");
 
 
 //    char *pd = (char *)malloc(10);
@@ -49,8 +49,20 @@ int main()
 //    free(pd);
 
 
-    int *pe = (int *)calloc(10, sizeof(int));
+    int *pe = (int *)calloc(2, sizeof(int));
     pe[0] = 1;
+    pe[1] = 3;
+    pe[2] = 3;
     printf("pe=%d\n",*pe+0);
+    printf("pe=%d\n",*(pe+1));
+    printf("pe=%d\n",*(pe+2));
+
+    int pei;
+    for(pei=0;pei<100;pei++){
+        pe[pei] = pei;
+    }
+    printf("pe[80]=%d\n",pe[80]);
+    pe[1024*1024] = 1024;
+    printf("pe[1024=%d\n",pe[1024*1024]);
     return 0;
 }
