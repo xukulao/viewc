b09eaf948941cbac73909948750f448da50bfb47
jackchensm <1655664358@qq.com>
2019-05-09 09:22:34 +0800

    chore():editor

--- module/test.c
+++ module/test.c
@@ -8,22 +8,45 @@
 void pause(char msg[]);
 char showMenu();
 void test();
+struct STU{
+    int id;
+    char name[20];
+    char sex[4];
+};
+typedef struct _NODE{
+    struct STU data;
+    struct _NODE *next;
+} NODE;
 int main()
 {
     int id;
-    while(1){
-        id = showMenu();
-        switch (id){
-            case '1':
-                test();
-                break;
-            case '2':
-                printf("hello\n");
-                pause("enter any key return ...");
-                break;
-            case '0':exit(EXIT_SUCCESS);
-        }
-    }
+//    while(1){
+//        id = showMenu();
+//        switch (id){
+//            case '1':
+//                test();
+//                break;
+//            case '2':
+//                printf("hello\n");
+//                pause("enter any key return ...");
+//                break;
+//            case '0':exit(EXIT_SUCCESS);
+//        }
+//    }
+
+    int *age = (int *)malloc(10* sizeof(int));
+    age[0] = 1;
+    age[1] = 2;
+    printf("age[0]=%d,age[0]=%d\n",age[0],*age);
+    printf("age=%d,age[0]=%d\n", sizeof(age[0]), sizeof(age[1]));
+
+    NODE node1;
+    node1.data.name[0] = 'j';
+    node1.data.sex[0] = 'n';
+    node1.data.id = 100;
+    printf("node=%d\n",node1.data.id);
+    node1.next = &node1;
+    printf("node2=%d\n",node1.next->id);
     return 0;
 }
 
