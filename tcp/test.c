5af54dbfed6eef04a604ba9cabc231d045079ef1
immortalChensm <1655664358@qq.com>
2019-05-17 00:12:18 +0800

    docs(tcp):append big and small endian

--- tcp/test.c
+++ tcp/test.c
@@ -3,7 +3,16 @@
 //
 
 #include <stdio.h>
+union{
+    int n;
+    char ch;
+    short int m;
+}data;
 void main()
 {
+    data.n = 0x00000001;
+    printf("data.n=%#X\n",data.n);
+    printf("data.ch=%d\n",data.ch);
+    printf("data.m=%d\n",data.m);
     printf("%f.5",1/3);
 }
