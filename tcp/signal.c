91254e319a1c0c7eb0edfc23b98d39a8179086d4
immortalChensm <1655664358@qq.com>
2019-05-21 22:00:44 +0800

    test(tcp):test bzero of string.h function

--- tcp/signal.c
+++ tcp/signal.c
@@ -3,9 +3,11 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
+#include <stdlib.h>//标准库
+#include <signal.h>//信号
+#include <unistd.h>//时间
+#include <assert.h>//声明
+#include <string.h>
 void sigHandler(int signo);
 
 int main(int argc,char *argv[])
@@ -18,7 +20,11 @@ int main(int argc,char *argv[])
 
     int age = atoi(argv[1]);
     printf("age=%d\n",age);
-    getchar();
+    //getchar();
+   // assert(1);
+    int k=100;
+    printf("k=%d\n",*&k);
+    bzero(&k, sizeof(k));//清空
     return 0;
 }
 
