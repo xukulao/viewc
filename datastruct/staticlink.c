c6a930a962646cebeb58744a6bae7dbc4f7db210
jackchensm <1655664358@qq.com>
2019-05-21 11:06:39 +0800

    test(link):static link table malloc init

--- datastruct/staticlink.c
+++ datastruct/staticlink.c
@@ -19,16 +19,24 @@ Status InitList(StaticLinkList space);
 int Malloc_SLL(StaticLinkList space);
 int main()
 {
-    Component test;
-    test.data=100;
-    test.cur = 1;
-    printf("test.data=%d\n",test.data);
+//    Component test;
+//    test.data=100;
+//    test.cur = 1;
+//    printf("test.data=%d\n",test.data);
+//
+//    StaticLinkList person;
+//    person[0].data = 1000;
+//    person[0].cur = 1;
+//    person[1].data = 2000;
+//    printf("person[1].data=%d\n",person[1].data);
 
-    StaticLinkList person;
-    person[0].data = 1000;
-    person[0].cur = 1;
-    person[1].data = 2000;
-    printf("person[1].data=%d\n",person[1].data);
+    StaticLinkList space;
+    InitList(space);
+    printf("%d\n",Malloc_SLL(space));
+    printf("%d\n",Malloc_SLL(space));
+    printf("%d\n",Malloc_SLL(space));
+    printf("%d\n",Malloc_SLL(space));
+
     return 0;
 }
 
@@ -40,14 +48,18 @@ int main()
 Status InitList(StaticLinkList space)
 {
     int i;
-    for(i=0;i<MAXSIZE;i++){
-        space[i].cur = i+1;
+    for(i=0;i<MAXSIZE-1;i++){
+        space[i].cur = (i+1);
     }
-    space[MAXSIZE-i].cur = 0;
+    space[MAXSIZE-1].cur = 0;
     return OK;
 }
 
 int Malloc_SLL(StaticLinkList space)
 {
-
+    int i = space[0].cur;//得到第一个元素的下标  i =1  space[0].cur=2 i =2 space[0].cur=3 i=3
+    if(space[0].cur){
+        space[0].cur = space[i].cur;//得到第二个元素的下标 假设i为第一个元素，它的cur保存的是第二个元素的下标  space[1].cur=2 space[2].cur=3
+    }
+    return i;
 }
