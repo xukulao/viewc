8b3ae16b1ec8ad8982a77bf935257c0f1ad3d05b
immortalChensm <1655664358@qq.com>
2019-05-21 21:48:57 +0800

    test(tcp):test atoi function

--- tcp/signal.c
+++ tcp/signal.c
@@ -3,17 +3,22 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
 void sigHandler(int signo);
 
-int main()
+int main(int argc,char *argv[])
 {
-    signal(SIGINT,sigHandler);
-    while(1)
-    {
-        sleep(1);
-    }
+//    signal(SIGINT,sigHandler);
+//    while(1)
+//    {
+//        sleep(1);
+//    }
+
+    int age = atoi(argv[1]);
+    printf("age=%d\n",age);
+    getchar();
     return 0;
 }
 
