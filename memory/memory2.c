50805a71e83e0652fe66a510651cd18999e34858
jackCsm <35517798+immortalChensm@users.noreply.github.com>
2020-04-12 20:17:24 +0800

    Update memory2.c

--- memory/memory2.c
+++ memory/memory2.c
@@ -11,7 +11,7 @@ int main()
 //    age = (int *)malloc(10* sizeof(int));
 //    *(age+1)=100;
 //    printf("age=%d\n",age[1]);
-
+    printf("age=%#lx\n",age[1]);
 //运行出错  name为指针变量  存在栈内存区域
 //    char *name = NULL;
 //    gets(name);
